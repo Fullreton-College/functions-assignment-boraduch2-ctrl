343c550cabfa46bd549433e8dfc5ade8b068a82c
Duch, Bora <boraduch2@gmail.com>
2026-03-04 18:14:38 -0800

    Completed Function_1 coin flip program

--- Function_1.cpp
+++ Function_1.cpp
@@ -3,13 +3,31 @@
 using namespace std;
 
 /* Define your function here */ 
+string CoinFlip() {
+   int value = rand() % 2;
 
+   if(value == 1) {
+      return "Heads";
+   }
+   else {
+      return "Tails";
+   }
+}
 int main() {
-   // Add more variables as needed
+    int flips;
+    srand(2);  // Unique seed
+    
+    cin >> flips;
+
+    for (int i = 0; i < flips; i++) {
+         cout << CoinFlip();
+         
+         if (i < flips - 1) {
+             cout << " ";
+         }
+      }
    
-   srand(2);  // Unique seed
-
-   /* Type your code here */
-
+   cout << endl;
+   
    return 0;
 }
