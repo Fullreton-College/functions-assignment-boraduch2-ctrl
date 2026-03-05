900d223755b721f206925a3d414e6b09c877e977
Duch, Bora <boraduch2@gmail.com>
2026-03-04 19:51:27 -0800

    Completed coin change program

--- Function_4.cpp
+++ Function_4.cpp
@@ -2,12 +2,39 @@
 using namespace std;
 
 //define necessary functions
+void computeCoins(int coinValue, int& num, int& amountLeft);
+
 int main()
 {
+    int cents;
+    int quarters, dimes, pennies;
+    char ans = 'y';
 
-  //your code goes here
+    while(ans == 'y' || ans == 'Y')
+    {
+        cout << "Enter change amount (1-99 cents): ";
+        cin >> cents;
 
-return 0;
+        int amountLeft = cents;
+
+        computeCoins(25, quarters, amountLeft);
+        computeCoins(10, dimes, amountLeft);
+        computeCoins(1, pennies, amountLeft);
+
+        cout << cents << " cents can be given as "
+             << quarters << " quarter(s) "
+             << dimes << " dime(s) and "
+             << pennies << " penny(pennies)" << endl;
+        
+        cout << "Enter Y or y to continue, any other halts\n\n";
+        cin >> ans;
+    }
+
+    return 0;
 }
 
-//implement functions
+void computeCoins(int coinValue, int& num, int& amountLeft)
+{
+    num = amountLeft / coinValue;
+    amountLeft = amountLeft % coinValue;
+}
